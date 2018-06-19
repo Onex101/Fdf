bfb4b8fd3d2e8a962c610db8be153dee79210ec0
Onex101 <xeno@zarobi.co.za>
2018-06-19 07:19:12 +0200

    Draw line function works with ints

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: xrhoda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 08:15:42 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/06/18 13:44:36 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/06/19 06:14:42 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ typedef struct	s_struct
 	void 		*mlx;
 	void 		*window;
 	void 		*map;
-
 }				t_struct;
 
-typedef	struct	s_struct
+typedef	struct	s_line
 {
-	int x1;
-	int	y1;
-	int x2;
-	int y2
-}				t_line
+	int 		x1;
+	int			y1;
+	int 		x2;
+	int 		y2;
+	t_struct	*box;
+}				t_line;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xrhoda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/15 12:47:32 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/06/18 13:52:06 by xrhoda           ###   ########.fr       */
+/*   Updated: 2018/06/19 06:52:49 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 #include <stdlib.h>
 #include <mlx.h>
 #include "fdf.h"
+#include <stdio.h>
 
 int ft_abs(int i)
 {
-	(i < 0) ? return(i * -1) : return (i);
+	if (i < 0)
+		return(i * -1);
+	return (i);
 }
 
 // int	draw_line(t_struct *win)
@@ -41,16 +44,14 @@ int ft_abs(int i)
 // 	return (0);
 // }
 
-
-
-void draw_line (t_line *line)
+int draw_line(t_line *line)
 {
 	int dx;
 	int dy;
 	int sx;
 	int sy;
 	int err;
-	int err2;
+	int m;
 
 	dx =  ft_abs (line->x2 - line->x1);
 	dy = -ft_abs (line->y2 - line->y1);
@@ -59,21 +60,55 @@ void draw_line (t_line *line)
 	err = dx + dy;
 	while (1)
 	{
-		mlx_pixel_put((line->win->mlx), (line->win->window), x1, y1, 0xFFFFFF);
-		if (x1 == x2 && y1 == y2) 
+		printf("Hi");
+		mlx_pixel_put((line->box->mlx), (line->box->window), line->x1, line->y1, 0xFFFFFF);
+		if (line->x1 == line->x2 && line->y1 == line->y2) 
 			break;
-		e2 = 2 * err;
-		if (e2 >= dy)
+		m = 2 * err;
+		if (m >= dy)
 		{ 
 			err += dy; 
-			x1 += sx; 
+			line->x1 += sx; 
 		}
-			if (e2 <= dx)
+		if (m <= dx)
 		{
 			err += dx; 
-			y1 += sy; 
+			line->y1 += sy; 
 		}
 	}
+	return (0);
+}
+
+int clock(t_line *line)
+{
+	int points;
+
+	points = 2000;
+	// while (points > 1500)
+	// {
+	// 	draw_line(line);
+	// 	points--;
+	// 	line->x1 += 1;
+	// }
+	while (points > 1000)
+	{
+		draw_line(line);
+		points--;
+		line->y1 += 1;
+	}
+	while (points > 500)
+	{
+		draw_line(line);
+		points--;
+		line->x1 -= 1;
+	}
+	while (points)
+	{
+		draw_line(line);
+		points--;
+		line->y1 -= 1;
+	}
+	return (0);
 }
 
 int	main(void)
@@ -87,8 +122,12 @@ int	main(void)
 		return (-1);
 	win->mlx = mlx_init();
 	win->window = mlx_new_window ((win->mlx) , 500, 500, "New Window" );
-
-	mlx_loop_hook((win->mlx), &(draw_line), (void *)win);	
+	line->box = win;
+	line->x1 = 0;
+	line->x2 = 250;
+	line->y1 = 0;
+	line->y2 = 250;
+	mlx_loop_hook((win->mlx), &(clock), (void *)line);	
 	//mlx_key_hook ( win->window, &draw_line, (void *)win);
 	mlx_loop (win->mlx);
 	return (0);
